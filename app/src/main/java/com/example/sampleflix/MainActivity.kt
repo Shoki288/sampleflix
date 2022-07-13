699d44ad395a7a1c436c0ffa1c9317f355408dd6
syooki <gsyoki0719@gmail.com>
2022-07-14 00:22:41 +0900

    Initial commit

---
+++ app/src/main/java/com/example/sampleflix/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.sampleflix
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val navHost = supportFragmentManager.findFragmentById(R.id.nav_host) as NavHostFragment
+        findViewById<BottomNavigationView>(R.id.bottom_nav).setupWithNavController(navHost.navController)
+    }
+}
