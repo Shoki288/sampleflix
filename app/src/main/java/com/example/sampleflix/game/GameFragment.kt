40896d9bf6499b04fe12b0cb30c28dd53e8edc41
syooki <gsyoki0719@gmail.com>
2022-07-14 00:36:15 +0900

    resource作成_package分け

--- app/src/main/java/com/example/sampleflix/game/GameFragment.kt
+++ app/src/main/java/com/example/sampleflix/game/GameFragment.kt
@@ -1,10 +1,11 @@
-package com.example.sampleflix
+package com.example.sampleflix.game
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.sampleflix.R
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -13,10 +14,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [NoticeFragment.newInstance] factory method to
+ * Use the [GameFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class NoticeFragment : Fragment() {
+class GameFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -34,7 +35,7 @@ class NoticeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_notice, container, false)
+        return inflater.inflate(R.layout.fragment_game, container, false)
     }
 
     companion object {
@@ -44,12 +45,12 @@ class NoticeFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment NoticeFragment.
+         * @return A new instance of fragment GameFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            NoticeFragment().apply {
+            GameFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
