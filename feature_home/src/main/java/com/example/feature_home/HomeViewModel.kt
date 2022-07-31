71f2214fc6e3536f6a6fc1abf95caac0a8b17023
syooki <gsyoki0719@gmail.com>
2022-07-31 22:03:43 +0900

    グロナビ上の画面の変更修正

--- feature_home/src/main/java/com/example/feature_home/HomeViewModel.kt
+++ feature_home/src/main/java/com/example/feature_home/HomeViewModel.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.viewModelScope
 import com.example.extension.api.AppException
 import com.example.extension.api.HttpError
 import com.example.extension.api.Success
+import com.example.feature_home.HomeUiState.ApiError
 import com.example.model.BookInfo
 import com.example.search_repository.usecase.SearchBookUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -17,76 +18,69 @@ class HomeViewModel @Inject constructor(
     private val useCase: SearchBookUseCase
 ) : ViewModel() {
 
-    private val _books = MutableStateFlow<HomeUiState>(HomeUiState.Loading)
-    val books = _books.asStateFlow()
+    private val books = MutableStateFlow<HomeUiState>(HomeUiState.Loading)
 
     init {
         viewModelScope.launch {
-            when (val res = useCase.searchBookInit()) {
-                is Success -> {
-                    _books.value = HomeUiState.Success(res.data.items)
-                }
-                is HttpError -> {
-                    _books.value = HomeUiState.ApiError(res.message)
-                }
-                is AppException -> {
-                    _books.value =
-                        HomeUiState.ApiError(res.e.message ?: "接続できませんでした。もう一度時間をおいて確認してください。")
-                }
-                else -> {
-                    _books.value = HomeUiState.ApiError("接続できませんでした。もう一度時間をおいて確認してください。")
-                }
+            books.value = when (val res = useCase.searchBookInit()) {
+                is Success -> HomeUiState.Success(res.data.items)
+                is HttpError -> ApiError(res.message)
+                is AppException -> ApiError(res.e.message ?: "接続できませんでした。もう一度時間をおいて確認してください。")
+                else -> ApiError("接続できませんでした。もう一度時間をおいて確認してください。")
             }
         }
 
-        viewModelScope.launch { _books.collect() }
+        viewModelScope.launch { books.collect() }
     }
 
 
     // 読み始めたシリーズを続ける
-    val recentlyReadingBooks = _books.filterIsInstance<HomeUiState.Success>().map {
+    val recentlyReadingBooks = books.filterIsInstance<HomeUiState.Success>().map {
         it.books.subList(0, 10)
     }.stateIn(viewModelScope, SharingStarted.Eagerly, emptyList())
 
     // すぐ読める本
-    val recommendBooks = _books.filterIsInstance<HomeUiState.Success>().map {
+    val recommendBooks = books.filterIsInstance<HomeUiState.Success>().map {
         it.books.subList(11, 20)
     }.stateIn(viewModelScope, SharingStarted.Eagerly, emptyList())
 
     // プライム会員特定で読めるベストセラー
-    val bestSellerBooks = _books.filterIsInstance<HomeUiState.Success>().map {
+    val bestSellerBooks = books.filterIsInstance<HomeUiState.Success>().map {
         it.books.subList(21, 30)
     }.stateIn(viewModelScope, SharingStarted.Eagerly, emptyList())
 
     // 最近読んだ本に基づくおすすめ
-    val recentlyReadHistoryBooks = _books.filterIsInstance<HomeUiState.Success>().map {
+    val recentlyReadHistoryBooks = books.filterIsInstance<HomeUiState.Success>().map {
         it.books.subList(31, 40)
     }.stateIn(viewModelScope, SharingStarted.Eagerly, emptyList())
 
     // もうすぐ読み放題が終了するタイトル
-    val endUnlimitedReadingBooks = _books.filterIsInstance<HomeUiState.Success>().map {
+    val endUnlimitedReadingBooks = books.filterIsInstance<HomeUiState.Success>().map {
         it.books.subList(41, 50)
     }.stateIn(viewModelScope, SharingStarted.Eagerly, emptyList())
 
     // 近日配信開始のタイトルのおすすめ
-    val recentlyReleaseBooks = _books.filterIsInstance<HomeUiState.Success>().map {
+    val recentlyReleaseBooks = books.filterIsInstance<HomeUiState.Success>().map {
         it.books.subList(51, 60)
     }.stateIn(viewModelScope, SharingStarted.Eagerly, emptyList())
 
     // 類似タイトルに基づくおすすめ
-    val similarTitleBooks = _books.filterIsInstance<HomeUiState.Success>().map {
+    val similarTitleBooks = books.filterIsInstance<HomeUiState.Success>().map {
         it.books.subList(61, 70)
     }.stateIn(viewModelScope, SharingStarted.Eagerly, emptyList())
 
     // 読書履歴に基づくおすすめ
     val readingHistoryBooks: StateFlow<List<BookInfo>> =
-        _books.filterIsInstance<HomeUiState.Success>().map {
+        books.filterIsInstance<HomeUiState.Success>().map {
             it.books.subList(71, 79)
         }.stateIn(viewModelScope, SharingStarted.Eagerly, emptyList())
 
     // 本をさらに見る
-    val categories = _books.filterIsInstance<HomeUiState.Success>().map {
-        val categories = it.books.map { books -> books.bookInfo.categories }.flatten().filterNot { list -> list.isEmpty() }.distinct()
+    val categories = books.filterIsInstance<HomeUiState.Success>().map {
+        // APIから取得したデータのカテゴリを吸い取っている
+        val categories = it.books.map { books -> books.bookInfo.categories }.flatten().apply {
+            filterNot { list -> list.isEmpty() }.distinct()
+        }
         categories.subList(0, 9)
     }.stateIn(viewModelScope, SharingStarted.Eagerly, emptyList())
 }
